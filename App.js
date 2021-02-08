ea0187cd53664755a11bd01ab5db7833991a656e
Anik7303 <anik7303@gmail.com>
2021-02-08 20:39:57 +0600

    using @react-native-community/hooks

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    StatusBar as ReactNativeStatusBar,
+    Platform,
+    SafeAreaView,
+} from "react-native";
+import {
+    useDimensions,
+    useDeviceOrientation,
+} from "@react-native-community/hooks";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const { landscape } = useDeviceOrientation();
+    console.log({ landscape });
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar style="auto" />
+            <Text style={styles.text}>Welcome to React Native with Expo</Text>
+            <View
+                style={{
+                    backgroundColor: "red",
+                    width: "100%",
+                    height: landscape ? "100%" : "30%",
+                }}
+            ></View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        // alignItems: "center",
+        // justifyContent: "center",
+        paddingTop:
+            Platform.OS === "android" ? ReactNativeStatusBar.currentHeight : 0,
+    },
+    text: {
+        // width: 150,
+        color: "dodgerblue",
+    },
 });
