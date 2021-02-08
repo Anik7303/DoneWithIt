2d0d3bb8e430d9fa87ee57666829c91c4f551cf7
Anik7303 <anik7303@gmail.com>
2021-02-08 23:22:14 +0600

    using border styles

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import ViewImage from "./app/screens/ViewImage";
-import WelcomeScreen from "./app/screens/Welcome";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
     return (
-        <>
-            <StatusBar style="auto" />
-            {/* <WelcomeScreen /> */}
-            <ViewImage />
-        </>
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <View
+                style={{
+                    backgroundColor: "dodgerblue",
+                    width: 100,
+                    height: 100,
+                    borderWidth: 10,
+                    borderColor: "royalblue",
+                    borderRadius: 10,
+                    borderTopRightRadius: 35,
+                    borderBottomLeftRadius: 35,
+                }}
+            ></View>
+        </View>
     );
 }
