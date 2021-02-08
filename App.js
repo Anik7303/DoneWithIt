0af8b4e5009e36c673dd000cc63501ac1ed193a7
Anik7303 <anik7303@gmail.com>
2021-02-08 23:35:20 +0600

    using margin, padding & text styles

--- App.js
+++ App.js
@@ -1,31 +1,70 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
+        <View style={{ flex: 1, justifyContent: "space-between" }}>
             <View
                 style={{
-                    backgroundColor: "dodgerblue",
-                    width: 100,
-                    height: 100,
-                    // shadow.. only works for ios
-                    shadowColor: "grey",
-                    shadowOffset: {
-                        width: 10,
-                        height: 10,
-                    },
-                    shadowOpacity: 1,
-                    // shadow's replacement in android is elevation
-                    elevation: 15,
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    padding: 15,
                 }}
-            ></View>
+            >
+                <Text
+                    style={{
+                        fontSize: 30,
+                        fontFamily:
+                            Platform.OS === "android" ? "Roboto" : "Courier",
+                        fontWeight: "700",
+                        fontStyle: "italic",
+                        color: "tomato",
+                        textTransform: "capitalize",
+                        textDecorationLine: "line-through",
+                        // textDecorationStyle: "dotted", //ios only
+                        // textDecorationColor: "black", // ios only
+                        lineHeight: 35,
+                    }}
+                >
+                    I love React Native! This is my first app created with react
+                    native. And i am loving it very much.
+                </Text>
+            </View>
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <View
+                    style={{
+                        backgroundColor: "dodgerblue",
+                        width: 100,
+                        height: 100,
+                        padding: 20,
+                        // marginBottom: 10,
+                    }}
+                >
+                    <View
+                        style={{
+                            width: 50,
+                            height: 50,
+                            backgroundColor: "gold",
+                            margin: 5,
+                        }}
+                    ></View>
+                </View>
+                <View
+                    style={{
+                        width: 100,
+                        height: 100,
+                        backgroundColor: "yellowgreen",
+                        marginTop: 10,
+                    }}
+                ></View>
+            </View>
         </View>
     );
 }
