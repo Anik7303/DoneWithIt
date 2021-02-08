0d72e8b413e07c81be98a73838d4973f61a0b63d
Anik7303 <anik7303@gmail.com>
2021-02-08 20:54:43 +0600

    using flex

--- App.js
+++ App.js
@@ -1,47 +1,63 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-    StyleSheet,
-    Text,
-    View,
-    StatusBar as ReactNativeStatusBar,
-    Platform,
-    SafeAreaView,
-} from "react-native";
-import {
-    useDimensions,
-    useDeviceOrientation,
-} from "@react-native-community/hooks";
+import { View } from "react-native";
 
 export default function App() {
-    const { landscape } = useDeviceOrientation();
-    console.log({ landscape });
     return (
-        <SafeAreaView style={styles.container}>
-            <StatusBar style="auto" />
-            <Text style={styles.text}>Welcome to React Native with Expo</Text>
+        <View
+            style={{
+                backgroundColor: "#fff",
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                alignContent: "center",
+                flexWrap: "wrap",
+            }}
+        >
             <View
                 style={{
-                    backgroundColor: "red",
-                    width: "100%",
-                    height: landscape ? "100%" : "30%",
+                    backgroundColor: "dodgerblue",
+                    width: 100,
+                    height: 100,
+                    // alignSelf: "flex-start",
+                    // flexBasis: 150,
+                    // flex: 1,
+                    // flexGrow: 1,
+                    // flexShrink: 1,
+                    top: 20,
                 }}
-            ></View>
-        </SafeAreaView>
+            />
+            <View
+                style={{
+                    backgroundColor: "yellowgreen",
+                    width: 100,
+                    height: 150,
+                    // flex: 1,
+                    // flexGrow: 1,
+                }}
+            />
+            <View
+                style={{
+                    backgroundColor: "tomato",
+                    width: 100,
+                    height: 200,
+                }}
+            />
+            <View
+                style={{
+                    backgroundColor: "gold",
+                    width: 100,
+                    height: 150,
+                }}
+            />
+            <View
+                style={{
+                    backgroundColor: "grey",
+                    width: 100,
+                    height: 100,
+                }}
+            />
+        </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        // alignItems: "center",
-        // justifyContent: "center",
-        paddingTop:
-            Platform.OS === "android" ? ReactNativeStatusBar.currentHeight : 0,
-    },
-    text: {
-        // width: 150,
-        color: "dodgerblue",
-    },
-});
