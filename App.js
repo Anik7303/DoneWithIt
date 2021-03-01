8abd0aa3185990dc5aa2b6e8207ff21143567a52
Anik7303 <anik7303@gmail.com>
2021-03-01 16:46:53 +0600

    using AsyncStorage

--- App.js
+++ App.js
@@ -1,31 +1,58 @@
 import React from "react"
-// import { useEffect, useState } from "react"
-import { Text } from "react-native"
-import { useNetInfo } from "@react-native-community/netinfo"
-// import NetInfo from "@react-native-community/netinfo"
+import { useEffect, useState } from "react"
+import { StyleSheet, View, Text, Button } from "react-native"
+import Constants from "expo-constants"
+// import AsyncStorage from "@react-native-async-storage/async-storage"
+import { useAsyncStorage } from "@react-native-async-storage/async-storage"
 
 const App = () => {
-    const netInfo = useNetInfo()
-    console.log({ netInfo })
+    const { getItem, setItem } = useAsyncStorage("person")
+    const [person, setPerson] = useState(null)
 
-    // NetInfo.fetch().then(netInfo => console.log({netInfo}))
+    const getPerson = async () => {
+        const item = await getItem()
+        setPerson(JSON.parse(item))
+    }
 
-    // const [netInfo, setNetInfo] = useState(null)
+    const updatePerson = async (newPerson) => {
+        await setItem(JSON.stringify(newPerson))
+        setPerson(newPerson)
+    }
 
-    // console.log(netInfo)
+    useEffect(() => {
+        getPerson()
+    }, [getPerson])
+
+    // const [person, setPerson] = useState(null)
+
+    // const demo = async () => {
+    //     await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }))
+    //     const person = await AsyncStorage.getItem("person")
+    //     console.log({ person })
+    // }
 
     // useEffect(() => {
-    //     let unsubscribe
-    //     ;(async () => {
-    //         unsubscribe = NetInfo.addEventListener((state) => setNetInfo(state))
-    //     })()
+    //     demo()
+    // }, [demo])
 
-    //     return () => {
-    //         if (unsubscribe) unsubscribe()
-    //     }
-    // }, [])
-
-    return <Text>App</Text>
+    return (
+        <View style={styles.container}>
+            <Text>Person id: {person?.id}</Text>
+            <Button
+                title="Update"
+                onPress={() =>
+                    updatePerson({ id: Math.floor(Math.random() * 100) })
+                }
+            />
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: Constants.statusBarHeight,
+    },
+})
 
 export default App
